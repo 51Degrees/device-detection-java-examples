2093e17133c39bb579bc02e4a91a4eeb4abb7806
Alexander Popel <alexander.popel@postindustria.com>
2025-05-21 10:09:21 +0300

    ci: update scripts to reflect common-ci changes

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,18 +1,17 @@
-
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
-    [Parameter(Mandatory=$true)]
-    [string]$DeviceDetection,
+    [Parameter(Mandatory=$true)][string]$RepoName,
+    [Parameter(Mandatory=$true)][string]$DeviceDetection,
     [string]$DeviceDetectionUrl
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
 ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
 
 Write-Output "Download Lite file"
-curl -L -o " $RepoPath/device-detection-data/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
+curl -L -o "$RepoPath/device-detection-data/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
 
 Write-Output "Download Evidence file"
 curl -o "$RepoPath/device-detection-data/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml"    
